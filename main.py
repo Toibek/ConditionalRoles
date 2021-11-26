1d2947de02b051fa14d223ccff32a9eb336954f8
Toby Oak <54889112+Toibek@users.noreply.github.com>
2021-11-26 23:40:30 +0000

    Getting started with parsing commands and changing user roles

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 import discord
+from discord.ext import commands
+from replit import db
 import os
 import datetime
 
 client = discord.Client();
 @client.event 
-async def OnReady
+async def on_ready():
+  print("Booted")
+
+@client.event
+async def on_message(message):
+  msg = message.content
+  if not msg.startswith('!') or message.author == client.user:
+    return
+  msg = msg[1::]
+  print(message.flags)
+  if "combo" in msg:
+    split = msg.split(" ")
+    if len(split) == 4: 
+      roles = [
+        split[1][3:-1:],
+        split[2][3:-1:],
+        split[3][3:-1:]
+      ]
+      for role in roles:
+        print(role)
+  roletoadd = message.author.guild.get_role(int(roles[2]))
+  await message.author.add_roles(roletoadd)
+
+  print(message.role_mentions)
+  
+
+  
+client.run(os.environ['token'])
+
